db31138218fd06b7fa0a72cda436c614fe2ba2bd
Codeyro <git@codeyro.ru>
2026-02-10 23:30:35 +0300

    Change logging level

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telethon.tl.functions.account import UpdateProfileRequest
 
 
 # Настройка логирования
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.WARN,
                     filename="liveprofile.log",
                     filemode="w",
                     format='%(asctime)s %(levelname)s %(message)s',
