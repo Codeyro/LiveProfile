26b26837e2d77cd3f195df4d9089abb1b9f32902
Codeyro <git@codeyro.ru>
2026-02-24 22:30:47 +0300

    Improve network exceptions

--- main.py
+++ main.py
@@ -13,58 +13,55 @@ logging.basicConfig(level=logging.WARN,
                     datefmt='%Y.%m.%d %H:%M:%S')
 
 
-# Чтение учетных данных
-try:
-    api_id = int(os.getenv('API_ID', 0))
-    api_hash = os.getenv('API_HASH', '')
-    session_string = StringSession(os.getenv('SESSION_STRING', ''))
-except Exception as e:
-    logging.critical(f'Config data read error: {e}')
+# Конфигурация
+API_ID = int(os.getenv('API_ID', 0))
+API_HASH = os.getenv('API_HASH', '')
+SESSION_STRING = os.getenv('SESSION_STRING', '')
 
 
 # Изменение шрифта
 def butificate(name):
-    name = name.replace('0', '𝟎')
-    name = name.replace('1', '𝟏')
-    name = name.replace('2', '𝟐')
-    name = name.replace('3', '𝟑')
-    name = name.replace('4', '𝟒')
-    name = name.replace('5', '𝟓')
-    name = name.replace('6', '𝟔')
-    name = name.replace('7', '𝟕')
-    name = name.replace('8', '𝟖')
-    name = name.replace('9', '𝟗')
-    name = name.replace(':', ':')
-    return name
+    digits = "0123456789"
+    bold_digits = "𝟎𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖𝟗"
+    table = str.maketrans(digits, bold_digits)
+    return name.translate(table)
 
 
 async def main():
     # Подключаемся к серверам Telegram
     try:
         client = TelegramClient(
-        session_string,
-        api_id,
-        api_hash
+        StringSession(SESSION_STRING),
+        API_ID,
+        API_HASH
         )
         await client.start()
     except errors.rpcerrorlist.AuthKeyDuplicatedError as e:
         logging.critical(e)
     
     # Основной скрипт
-    i = 1
     name = None
     while True:
-        if datetime.now().strftime('%H:%M') != name:
-            try :
+        try :
+            if not client.is_connected():
+                await client.connect()
+
+            if datetime.now().strftime('%H:%M') != name:
                 name = datetime.now().strftime('%H:%M')
-                name_b = butificate(name)
-                await client(UpdateProfileRequest(first_name=name_b))
-                logging.debug(f'Name changed to "{name_b }"')
-            except errors.rpcerrorlist.FloodWaitError as e:
-                logging.error(f'Flood Wait Error {e.seconds} seconds')
-            except ConnectionError as e:
-                logging.error(e)
-            i += 1
+                
+            name_b = butificate(name)
+
+            await client(UpdateProfileRequest(first_name=name_b))
+            logging.debug(f'Name changed to "{name_b }"')
+        except errors.rpcerrorlist.FloodWaitError as e:
+            logging.error(f'Flood Wait Error {e.seconds} seconds')
+            await asyncio.sleep(e.seconds)
+        except (ConnectionError, OSError) as e:
+            logging.error(f'Network error: {e}. Retry to connect after 10 seconds...')
+            await asyncio.sleep(10)
+        except Exception as e:
+            logging.exception(f'Unexcepted error: {e}')
+            await asyncio.sleep(10)
 
 
 if __name__ == '__main__':
