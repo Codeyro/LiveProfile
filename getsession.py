173dbdc60cc92891a281fa3e86e0f51ad78897df
Codeyro <git@codeyro.ru>
2026-04-08 21:42:28 +0300

    Change the method of getting the configuration to the .env file

--- getsession.py
+++ getsession.py
@@ -1,30 +1,24 @@
+import os
 import logging
 import asyncio
-from configparser import ConfigParser
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO,
-                    filename="liveprofile.log",
-                    filemode="w",
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%Y.%m.%d %H:%M:%S')
 
-# Чтение учетныех данных
-try:
-    config = ConfigParser()
-    config.read('config.ini')
 
-    api_id = int(config['Telegram']['api_id'])
-    api_hash = config['Telegram']['api_hash']
-    phone = config['Telegram']['phone']
-except Exception as e:
-    logging.critical(f'Config data read error: {e}')
+# Конфигурация
+API_ID = int(os.getenv('API_ID', 0))
+API_HASH = os.getenv('API_HASH', '')
+SESSION_STRING = os.getenv('SESSION_STRING', '')
+
 
 async def main():
-    async with TelegramClient(StringSession(), api_id, api_hash) as client:
+    async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
         print("Session string:", client.session.save())
 
 if __name__ == '__main__':
